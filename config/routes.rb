b328274205a0a02fb6d3f5b3770a951de3a6b2b0
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2023-05-28 17:46:16 +0530

    removed extar code

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  get '/home/index', to: 'home#index'
+  # get '/home/index', to: 'home#index'
 
   resources :tweets, only: :create
   
