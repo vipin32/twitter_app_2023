6f5027513e30d6c818dfafb32289e573092eaea7
vipin <champvip32@gmail.com>
2023-07-04 16:29:29 +0530

    test cases for username and display name changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   validates :username, uniqueness: {case_sensitive: false}, allow_blank: true
 
-  before_save :set_display_name, if: -> { display_name.blank?}
+  before_save :set_display_name, if: -> { username.present? && display_name.blank?}
 
   def set_display_name
     self.display_name = username.humanize
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,27 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many(:tweets).dependent(:destroy) }
   it { should validate_uniqueness_of(:username).case_insensitive.allow_nil }
+
+  describe "#set_display_name" do
+    context "when display_name is set" do
+      it "does not change the display name" do
+        user = build(:user, username: "testuser", display_name: "Test User")
+
+        user.save
+
+        expect(user.reload.display_name).to eq("Test User")
+     
+      end
+
+    end
+
+    context "when display_name is not set" do
+      it "changes the display name" do
+        user = build(:user, username: "testuser", display_name: nil)
+        user.save
+        expect(user.reload.display_name).to eq("Testuser")
+      end
+    end
+
+  end
 end
